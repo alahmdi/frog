b8733661b7b0094a4b39783e1d2d48680bc448ba
yong <yong9981@gmail.com>
2022-09-26 00:48:43 -0600

    Add history\011, yinyan_eatfood

--- history/011_yinyan_eatfood/src/main/java/com/gitee/drinkjava2/frog/objects/EnvObject.java
+++ history/011_yinyan_eatfood/src/main/java/com/gitee/drinkjava2/frog/objects/EnvObject.java
@@ -1,5 +1,5 @@
-/*
- * Copyright 2018 the original author or authors. 
+/* Copyright 2018-2020 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by
@@ -8,25 +8,19 @@
  * OF ANY KIND, either express or implied. See the License for the specific
  * language governing permissions and limitations under the License.
  */
-package com.gitee.drinkjava2.frog.util;
+package com.gitee.drinkjava2.frog.objects;
 
 /**
- *  Cube has x,y,z value
- *  
+ * EnvObject means some virtual object in Env
+ * 
  * @author Yong Zhu
- * @since 2021-11-07
+ * @since 1.0
  */
-public class Cube {
-    public int x;//x,y,z是cube的左下前点坐标
-    public int y;
-    public int z;
-    public int size;//size是cube的边长
+public interface EnvObject {
 
-    public Cube(int x, int y, int z, int size) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-        this.size = size;
-    }
+	public void build(); // 在Env中创建本身物体，指改变Env.bricks数组元素为本身物体的组成材料。只在每屏测试前调用一次
 
+	public void destory();// 从Env中清除本身物体，只在每屏测试完成后调用一次
+
+	public void active(); // 每个步长都会调用一次这个方法
 }
